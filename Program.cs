9da2e03c3ae2b48de756fd52f3bfae78dd0fd8e0
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 14:03:38 +0530

    Add : Added Program File For AddressBookSystem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Contact c1 = new Contact("Pankaj", "Naik", "abc@gmail.com", "Marcos Colony", "Nigeria", "Lonavala", 401204, 9876543210);
+        }
+    }
+}
