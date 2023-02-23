dddaf03fbd4100632e1d22a246a7f2977b255d9c
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-23 18:33:26 +0530

    Add : Added Program File For AddressBookSystem

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            Contact c1 = new Contact();
-            String[] names = { "Sagar", "Krishna", "Pankaj", "Dharani" };
-            c1.change(names);
+            //Contact c1 = new Contact();
+            //String[] names = { "Sagar", "Krishna", "Pankaj", "Dharani" };
+            //c1.change(names);
+
+            //CreateContactBuiltArray createContactBuiltArray = new CreateContactBuiltArray();
+            //createContactBuiltArray.addContact("sagar","shahu","nag","sss","sss","15544555","444454","sss@gmail.com");
+            //createContactBuiltArray.run();
+
+            AddressBookMain.AddContact();
+            AddressBookMain.Display();
+
         }
+
     }
 }
